5d974e139f58acd1b9a4333fbc30b3d82c744cb4
Malcolm Ramsay <malramsay64@gmail.com>
2019-09-21 09:35:19 +1000

    refactor: Switch to using structopt and paw for cli
    This uses the structopt and paw tools for command line argument parsing
    which hugely simplify the code which needs to be written to define and
    parse the arguments.
    
    This is primarily an exercise in trying out structopt and paw, with the
    conclusion that it is really simple, although there are complications
    with enums, though not that much more than already exists within clap.

--- src/main.rs
+++ src/main.rs
@@ -12,19 +12,41 @@ use rand_xorshift::XorShiftRng;
 use rand_xoshiro::Xoroshiro128StarStar;
 use sfmt::SFMT;
 
-use clap::{arg_enum, value_t, App, Arg};
+use clap::arg_enum;
+use structopt::StructOpt;
 
 arg_enum! {
-    #[derive(PartialEq, Debug)]
-    enum Generators {
-        ANSIC,
-        RANDU,
-        CPP,
-        TWISTER,
-        PCG,
-        XORSHIFT,
-        XOSHIRO,
-    }
+#[derive(PartialEq, Debug)]
+enum Generators {
+    ANSIC,
+    RANDU,
+    CPP,
+    TWISTER,
+    PCG,
+    XORSHIFT,
+    XOSHIRO,
+}
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "random_number_generators")]
+struct Args {
+    /// The psuedo random number generator to use
+    #[structopt(possible_values = &Generators::variants(), case_insensitive = true)]
+    generator: Generators,
+
+    /// The number of columns of data to output
+    #[structopt(short, long, default_value = "1")]
+    dimensions: usize,
+
+    /// The number of rows of data to generate
+    #[structopt(short, default_value = "10")]
+    num_samples: usize,
+
+    /// A number with which to seed the initial state of the pseudo-random number generator.
+    /// The default is to use system randomess to generate a state.
+    #[structopt(short, long)]
+    seed: Option<u64>,
 }
 
 /// Print randomly generated numbers to standard out
@@ -34,78 +56,60 @@ arg_enum! {
 /// - `samples`: How many lines of output to generate
 /// - `dimensions`: The number of points on each line to generate
 ///
-fn generate_numbers<R: Rng + ?Sized>(rng: &mut R, samples: u64, dimensions: u64) {
+fn generate_numbers<R: Rng + ?Sized>(rng: &mut R, samples: usize, dimensions: usize) {
     for _ in 0..samples {
         println!("{}", (0..dimensions).map(|_| rng.gen::<u32>()).format(", "));
     }
 }
 
-fn main() {
-    let matches = App::new("Random Number Generator")
-        .version("0.2.1")
-        .author("Malcolm Ramsay <malramsay64@gmail.com>")
-        .about("Generates random numbers using a range of different algorithms")
-        .arg(
-            Arg::with_name("rng")
-                .possible_values(&Generators::variants())
-                .case_insensitive(true)
-                .required(true)
-                .help("The pseudo random number generator to use."),
-        )
-        .arg(
-            Arg::with_name("dimensions")
-                .short("d")
-                .long("dimensions")
-                .default_value("1")
-                .takes_value(true)
-                .help("Number of columns in the output dataset."),
-        )
-        .arg(
-            Arg::with_name("num_samples")
-                .short("n")
-                .long("num-samples")
-                .default_value("10")
-                .takes_value(true)
-                .help("Number of rows of data to generate."),
-        )
-        .arg(
-            Arg::with_name("seed")
-                .short("s")
-                .long("seed")
-                .takes_value(true)
-                .help("The initial state of the psuedo random number generator"),
-        )
-        .get_matches();
-
-    let seed = match value_t!(matches, "num_samples", u64) {
-        Ok(s) => s,
+#[paw::main]
+fn main(args: Args) -> Result<(), Box<dyn std::error::Error>> {
+    let seed = match args.seed {
+        Some(s) => s,
         // When there is no given seed value, generate a truly random value
-        Err(_) => rand::rngs::OsRng.next_u64(),
+        None => rand::rngs::OsRng.next_u64(),
     };
-
-    let samples = value_t!(matches, "num_samples", u64).unwrap();
-    let dimensions = value_t!(matches, "dimensions", u64).unwrap();
-    let gen = value_t!(matches.value_of("rng"), Generators).unwrap_or_else(|e| e.exit());
 
     // Print out the random number generator we have chosen
-    println!("{:?}", gen);
+    println!("{:?}", args.generator);
 
     // Generate the random numbers using the selected random number generator.
-    match gen {
-        Generators::ANSIC => generate_numbers(&mut ANSIC::seed_from_u64(seed), samples, dimensions),
-        Generators::RANDU => generate_numbers(&mut RANDU::seed_from_u64(seed), samples, dimensions),
-        Generators::CPP => generate_numbers(&mut CPP::seed_from_u64(seed), samples, dimensions),
-        Generators::TWISTER => {
-            generate_numbers(&mut SFMT::seed_from_u64(seed), samples, dimensions)
-        }
-        Generators::PCG => generate_numbers(&mut Pcg64::seed_from_u64(seed), samples, dimensions),
-        Generators::XORSHIFT => {
-            generate_numbers(&mut XorShiftRng::seed_from_u64(seed), samples, dimensions)
-        }
+    match args.generator {
+        Generators::ANSIC => generate_numbers(
+            &mut ANSIC::seed_from_u64(seed),
+            args.num_samples,
+            args.dimensions,
+        ),
+        Generators::RANDU => generate_numbers(
+            &mut RANDU::seed_from_u64(seed),
+            args.num_samples,
+            args.dimensions,
+        ),
+        Generators::CPP => generate_numbers(
+            &mut CPP::seed_from_u64(seed),
+            args.num_samples,
+            args.dimensions,
+        ),
+        Generators::TWISTER => generate_numbers(
+            &mut SFMT::seed_from_u64(seed),
+            args.num_samples,
+            args.dimensions,
+        ),
+        Generators::PCG => generate_numbers(
+            &mut Pcg64::seed_from_u64(seed),
+            args.num_samples,
+            args.dimensions,
+        ),
+        Generators::XORSHIFT => generate_numbers(
+            &mut XorShiftRng::seed_from_u64(seed),
+            args.num_samples,
+            args.dimensions,
+        ),
         Generators::XOSHIRO => generate_numbers(
             &mut Xoroshiro128StarStar::seed_from_u64(seed),
-            samples,
-            dimensions,
+            args.num_samples,
+            args.dimensions,
         ),
     };
+    Ok(())
 }
